d7bd8a18f92813be7d5d71208c040463f4895ec7
kuztaf <kuztaf@gmail.com>
2025-08-01 17:05:47 +0200

    feat. Added env file

--- src/main/java/com/cinema/cinema/CinemaApplication.java
+++ src/main/java/com/cinema/cinema/CinemaApplication.java
@@ -3,10 +3,24 @@ package com.cinema.cinema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CinemaApplication {
 
 	public static void main(String[] args) {
+		// Cargar variables del archivo .env
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+		
+		// Establecer las variables como propiedades del sistema
+		dotenv.entries().forEach(entry -> 
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+		
 		SpringApplication.run(CinemaApplication.class, args);
 	}
 
