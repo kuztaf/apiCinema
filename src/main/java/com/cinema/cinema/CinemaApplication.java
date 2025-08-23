3cbdbf9b1d94745140908aecc9e4d7bcc488ffc3
Gustavo Cali <umbreon77@gmail.com>
2025-08-24 00:12:24 +0200

    feat. microservice configuration

--- src/main/java/com/cinema/cinema/CinemaApplication.java
+++ src/main/java/com/cinema/cinema/CinemaApplication.java
@@ -2,10 +2,12 @@ package com.cinema.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CinemaApplication {
 
 	public static void main(String[] args) {
@@ -15,12 +17,10 @@ public class CinemaApplication {
 				.ignoreIfMalformed()
 				.ignoreIfMissing()
 				.load();
-		
+
 		// Establecer las variables como propiedades del sistema
-		dotenv.entries().forEach(entry -> 
-			System.setProperty(entry.getKey(), entry.getValue())
-		);
-		
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(CinemaApplication.class, args);
 	}
 
